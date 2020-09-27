db7cbdc22499fc917bb10e58fd4719645a4df90d
daigo-program <mamushi.journey@gmail.com>
2020-09-27 14:43:55 +0900

    flutter web版のgoogle mapsを実装

---
+++ lib/map/map_model.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapStore with ChangeNotifier {
+  Completer<GoogleMapController> _controller = Completer();
+
+  onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+    notifyListeners();
+  }
+}
--- lib/map/map_page.dart
+++ lib/map/map_page.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:html';
+
+import 'package:provider/provider.dart';
+
+import 'map_model.dart';
 
 class MapPage extends StatelessWidget {
+  MapPage({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Text('マップページ');
+    return Scaffold(
+        body: ChangeNotifierProvider<MapStore>(
+      create: (_) => MapStore(),
+      child: GoogleMapState(),
+    ));
+  }
+}
+
+class GoogleMapState extends StatelessWidget {
+  GoogleMapState({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final mapstore = Provider.of<MapStore>(context);
+    return Container(
+      child: GoogleMap(
+          onMapCreated: mapstore.onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: LatLng(35.6580339, 139.7016358),
+            zoom: 16.0,
+          )),
+    );
   }
 }
